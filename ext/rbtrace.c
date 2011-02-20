3888caf5f2397b28c7d4e357ce2236eabef67692
Aman Gupta <aman@tmm1.net>
2011-02-19 19:56:21 -0800

    64 bytes isn't enough to do anything useful

--- ext/rbtrace.c
+++ ext/rbtrace.c
@@ -51,7 +51,7 @@ timeofday_usec()
 #define MAX_TRACERS 100 // max method tracers
 #define MAX_EXPRS 10    // max expressions per tracer
 #ifndef BUF_SIZE        // msgq buffer size
-#define BUF_SIZE 64
+#define BUF_SIZE 120
 #endif
 
 typedef struct {
